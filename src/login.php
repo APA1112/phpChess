abff9f9defa5f241dc4cf7b9c81cede8eff516a6
APATARA1112 <alejandropatonaraujo2@gmail.com>
2023-12-07 11:53:55 +0100

    creating login.php

---
+++ src/login.php
@@ -0,0 +1,50 @@
+<?php
+require_once 'config.php';
+global $db;
+
+session_start();
+if (isset($_POST['login'])) {
+    try {
+        $username = $_POST['username'];
+
+        $query = $db->prepare('SELECT * FROM player WHERE username=:user');
+        $query->bindValue(':user', $username, PDO::PARAM_STR);
+        $query->execute();
+
+        $user = $query->fetch(PDO::FETCH_ASSOC);
+        if ($user) {
+            if ($user['password'] === $_POST['pass']) {
+                $_SESSION['username'] = $_POST['username'];
+                header('Location: lobby.php');
+                die();
+            } else {
+                $error = 'Invalid username/password';
+            }
+        } else {
+            $error = 'Invalid username/password';
+        }
+    } catch (PDOException $e) {
+        $error = 'Database error: ' . $e->getMessage();
+    }
+}
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>OreChess</title>
+</head>
+<body>
+<h1>Login</h1>
+<?php
+if (!empty($error)) {
+    echo "<h2>" . $error . "</h2>";
+}
+?>
+<form method="post">
+    <input type="text" name="username" placeholder="Enter your username" />
+    <input type="password" name="pass" placeholder="Enter password" />
+    <button type="submit" name="login">Log in</button>
+</form>
+</body>
+</html>
