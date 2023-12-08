1f65c1f80a81b6a2af997b20ef07c5b958cc69c1
APATARA1112 <alejandropatonaraujo2@gmail.com>
2023-12-08 20:27:16 +0100

    Finished abstract class Piece.php

--- src/Piece.php
+++ src/Piece.php
@@ -1,5 +1,26 @@
 <?php
 
-abstract class Piece
-{
+abstract class Piece {
+    const WHITE = 0;
+    const BLACK = 1;
+
+    protected $color;
+    protected $board;
+
+    public function __construct($color, $board) {
+        $this->color = $color;
+        $this->board = $board;
+    }
+
+    public function getColor() {
+        return $this->color;
+    }
+
+    public function getBoard() {
+        return $this->board;
+    }
+
+    abstract public function canMove($fromRow, $fromCol, $toRow, $toCol);
+
+    abstract public function countMoves($fromRow, $fromCol);
 }
